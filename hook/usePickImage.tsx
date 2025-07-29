5177a48e37f7b72534816d9a505da02645dde33e
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-07-29 14:36:26 +0600

    update: image picker

--- hook/usePickImage.tsx
+++ hook/usePickImage.tsx
@@ -1,9 +1,10 @@
 import * as ImagePicker from "expo-image-picker";
 
-export const usePickImage = async () => {
-  const result = await ImagePicker.launchImageLibraryAsync({});
-  if (result.assets) {
-    return result.assets[0].uri;
-  }
-  return null;
-};
+export const usePickImage =
+  async (): Promise<ImagePicker.ImagePickerAsset | null> => {
+    const result = await ImagePicker.launchImageLibraryAsync({});
+    if (result.assets) {
+      return result.assets[0];
+    }
+    return null;
+  };
