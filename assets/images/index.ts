10d8a52eb0d472a3ec4e9208efa4fdae7b25c9c8
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-07-29 14:35:10 +0600

    add: fallback image for empty state

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -2,3 +2,7 @@ export const ImgWeather = require("./weatherImage.png");
 export const ImgCar = require("./car.png");
 export const ImgPassport = require("./take_image.png");
 export const ImgRocket = require("./rocket.png");
+
+export const ImageAssets = {
+  emptyImage: require("./emptyImage.png"),
+};
